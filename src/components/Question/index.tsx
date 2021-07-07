878437a7d7564a9ebe6b189c0ef0b7a76cba6763
Carlose <carlosviniwork@gmail.comm>
2021-07-07 11:29:47 -0300

    NLW#5 Together

---
+++ src/components/Question/index.tsx
@@ -0,0 +1,48 @@
+import { ReactNode } from 'react'
+import cx from 'classnames'
+import './style.scss';
+
+
+type QuestionProps = {
+    content: string;
+    author: {
+        name: string;
+        avatar: string;
+    };
+    children?: ReactNode;
+    isAnswered?: boolean;
+    isHighlighted?: boolean;
+}
+
+
+
+export function Question ({
+    content,
+    author,
+    children,
+    isHighlighted = false,
+    isAnswered = false,
+}: QuestionProps) {
+    return (
+        <div 
+            className={cx(
+                'question',
+                { answered: isAnswered },
+                { highlighted: isHighlighted && !isAnswered },
+            )}
+        >
+            <p>{content}</p>
+            <footer>
+                <div className="user-info">
+                    <img src={author.avatar} alt={author.name} />
+                    <span>{author.name}</span>
+                </div>
+
+                <div>
+                    {children}
+                </div>
+
+            </footer>
+        </div>
+    )
+}
